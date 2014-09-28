366930892549438572d5575339072a1cd04dbac7
Art Beatte <art.beatte@docusign.com>
2014-09-28 14:48:13 -0700

    Continues work on webpage tutorial. (currently broken).

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+    .config([
+        '$stateProvider',
+        '$urlRouteProvider',
+        function($stateProvider, $urlRouterProvider) {
+
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                })
+                .state('posts', {
+                    url: '/posts/{id}',
+                    templateUrl: '/posts.html',
+                    controller: 'PostsCtrl'
+                });
+
+            $urlRouterProvider.otherwise('home');
+        }
+    ])
+    .factory('posts', [function() {
+        var o = {
+            posts: []
+        };
+        return o;
+    }])
     .controller('MainCtrl', [
         '$scope',
-        function($scope){
+        'posts',
+        function($scope, posts){
 
             $scope.test = 'Hello world!';
 
-            $scope.posts = [
-                {title: 'post 1', upvotes: 5},
-                {title: 'post 2', upvotes: 2},
-                {title: 'post 3', upvotes: 15},
-                {title: 'post 4', upvotes: 9},
-                {title: 'post 5', upvotes: 4}
-            ];
+            $scope.posts = posts.posts;
 
             $scope.addPost = function(){
                 if($scope.title === '') { return; }
                 $scope.posts.push({
                     title: $scope.title,
                     link: $scope.link,
-                    upvotes: 0});
+                    upvotes: 0,
+                    comments: [
+                        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                    ]
+                });
                 $scope.title = '';
                 $scope.link = '';
             };
@@ -26,5 +52,11 @@ angular.module('flapperNews', [])
             $scope.incrementUpvotes = function(post) {
                 post.upvotes += 1;
             };
+        }])
+    .controller('PostsCtrl', [
+        '$scope',
+        '$stateParams',
+        '$posts',
+        function ($scope, $stateParams, posts) {
+            $scope.posts = posts.posts[$stateParams.id];
         }]);
-
